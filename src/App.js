483966389feb579629524ad6b07725e0b117ed37
unknown <leeviva@gmail.com>
2021-11-22 19:44:32 +0200

    added delete again

--- src/App.js
+++ src/App.js
@@ -14,6 +14,14 @@ function App() {
     setTodos([...todos, todo]);
   }
 
+  function addRemove (index)  {
+    const removable = index;
+    setTodos(todos.filter((todo, number) => number !== removable));
+  }
+
+
+  
+
 
   return (
     <div className="App">
@@ -21,7 +29,9 @@ function App() {
       <h1>To Do List</h1>
 
       <form onSubmit={addTodo}>
+		<label>Date: </label>
         <input type="date" name="date" onChange={inputChanged} value={todo.date}/>
+		<label> Description: </label>
         <input type="text" name="desc" onChange={inputChanged} value={todo.desc}/>
         <input type="submit" value="Add"/>
       </form>
@@ -30,7 +40,7 @@ function App() {
         <tbody>
           {todos.map((todo, index) => <tr key={index}>
             <td>{todo.desc}</td>
-            <td>{todo.date}</td>
+            <td>{todo.date}<button onClick={() => addRemove(index)}>Remove</button></td>
             </tr>)}
         </tbody>
       </table>
